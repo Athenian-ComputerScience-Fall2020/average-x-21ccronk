0d0d8ef3cbaac0c67002f93ed31077f8501f0e8e
21ccronk <21ccronk@athenian.org>
2020-09-28 01:28:18 +0000

    Fixed so chosen number of inputs is correct

--- my_code.py
+++ my_code.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     # test your fucntion with this print statement before writing the input loop
     #print(avg([0, 12, 3]))    # Put the values you want to test in for x,y and z
 
-    div = int(input("How many number would you like to take the average of?"))
+    div = int(input("How many number would you like to take the average of?: "))
    
     # Now, comment out the print statement and work on the code below
     # Remember to indent in this section
 
     user_list = [] # start with an empty list
     x = 0
-    while x <= div:
+    while x < div:
         y = int(input("Enter a number: "))
         x = x + 1
         user_list.append(y)
